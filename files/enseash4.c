39cba2034eeb078f2af9cf08655a88c6f02f215b
Younes-LH <youneslahjouji19@gmail.com>
2025-12-14 18:28:35 +0100

    Improve comments in enseash4.c for better clarity
    Updated comments for clarity and understanding.

--- files/enseash4.c
+++ files/enseash4.c
@@ -1,4 +1,4 @@
-#define _POSIX_C_SOURCE 199309L
+#define _POSIX_C_SOURCE 199309L // it activate clock_gettime()
 
 #include <unistd.h>
 #include <string.h>
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <time.h>
 
-#define BUF_SIZE 1024
+#define BUF_SIZE 1024 //standard size for buffer
 
 int main(void) {
     const char *welcome =
@@ -18,44 +18,55 @@ int main(void) {
     char buffer[BUF_SIZE];
 
    
-    int  has_status   = 0;     
-    int  last_exit    = 0;     
-    int  last_sign    = 0;     
-    long last_time_ms = 0;     
-    char prompt[64];          
+    int  has_status   = 0;     //flag
+    int  last_exit    = 0;     //it stores the return code 
+    int  last_sign    = 0;     //it stores signal number
+    long last_time_ms = 0;     //it stores the execution time of the previous command
+    char prompt[64];     //buffer for prompt     
 
     write(STDOUT_FILENO, welcome, strlen(welcome));
 
     while (1) {
-       
+       //If no command has been issued yet
         if (!has_status) {
             strcpy(prompt, "enseash % ");
-        } else if (last_sign != 0) {
+        } 
+       // if the previous command ended with a signal
+            
+        else if (last_sign != 0) {
             snprintf(prompt, sizeof(prompt),
                      "enseash [sign:%d|%ldms] %% ",
                      last_sign, last_time_ms);
-        } else {
+        } 
+        //otherwise she ended up normally
+        
+        else {
             snprintf(prompt, sizeof(prompt),
                      "enseash [exit:%d|%ldms] %% ",
                      last_exit, last_time_ms);
         }
+        
         write(STDOUT_FILENO, prompt, strlen(prompt));
        
 
-       
-        ssize_t n = read(STDIN_FILENO, buffer, BUF_SIZE - 1);
+       //Read what the user types:
+        ssize_t n = read(STDIN_FILENO, buffer, BUF_SIZE - 1); // n : number of characters read.
 
+        //ctrl +D
         if (n == 0) { 
             write(STDOUT_FILENO, bye, strlen(bye));
             break;
         }
+
+        // if read() failed
         if (n < 0) {  
             break;
         }
 
-        buffer[n] = '\0';
+        buffer[n] = '\0'; // to indicate the end of the character string
 
-        
+
+        // cleaning of \n, spaces, and tabs
         if (n > 0 && buffer[n - 1] == '\n') {
             buffer[n - 1] = '\0';
         }
@@ -72,32 +83,40 @@ int main(void) {
             continue;
         }
 
-       
+       // exit command:
+        // strcmp for comparison 
         if (strcmp(buffer, "exit") == 0) {
             write(STDOUT_FILENO, bye, strlen(bye));
             break;
         }
 
-        
+        //measurement of execution time
         struct timespec start, end;
         clock_gettime(CLOCK_MONOTONIC, &start);
         
 
-        pid_t pid = fork();
+        pid_t pid = fork(); // creation of the fork
 
+        //child process:
         if (pid == 0) {
            
             char *argv[2];
             argv[0] = buffer;
             argv[1] = NULL;
 
-            execvp(buffer, argv);
+            execvp(buffer, argv); // replaces the child program with the user's command 
 
             
-            _exit(127);
-        } else if (pid > 0) {
+            _exit(127); // exit with 127 if execvp failed
+            
+        } 
+        
+        //Parent process
+        
+        else if (pid > 0) {
             int status;
 
+            //it waits for the end of the child process with waitpid
             if (waitpid(pid, &status, 0) > 0) {
                 
                 clock_gettime(CLOCK_MONOTONIC, &end);
@@ -111,6 +130,7 @@ int main(void) {
                 last_exit  = 0;
                 last_sign  = 0;
 
+                //it retrieves the return code/signal: 
                 if (WIFEXITED(status)) {
                     last_exit = WEXITSTATUS(status);
                 } else if (WIFSIGNALED(status)) {
